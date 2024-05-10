1def20e58206abbf287006724d1ea07d7cd4342d
Adi Jain <adi1510jain@gmail.com>
2024-05-10 22:30:48 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,110 @@
+import pygame
+from fighter import Fighter
+
+pygame.init()
+
+#Creating game window
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Brawler")
+
+#set FrameRate
+
+clock = pygame.time.Clock()
+FPS = 60
+
+#define colors
+
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+
+#define fighter variables
+
+WARRIOR_SIZE = 162
+WARRIOR_SCALE = 4
+WARRIOR_OFFSET = [72, 56]
+WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]
+WIZARD_SIZE = 250
+WIZARD_SCALE = 3
+WIZARD_OFFSET = [112, 107]
+WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
+
+#load background image
+
+bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
+
+#load spritesheets
+
+warrior_sheet = pygame.image.load("assets/images/warrior/Sprites/warrior.png").convert_alpha()
+wizard_sheet = pygame.image.load("assets/images/wizard/Sprites/wizard.png").convert_alpha()
+
+# Define number of steps in each animation
+
+WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
+WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
+#function for drawing background
+
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0, 0))
+
+#Create two instances of fighter
+
+fighter_1 = Fighter(200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS)
+fighter_2 = Fighter(700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS)
+
+
+#function for drawing health bars
+
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
+
+#game loop
+
+run = True
+while run:
+    clock.tick(FPS)
+
+    #draw background
+
+    draw_bg()
+
+    #show player stats
+
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)    
+
+    #move fighters
+
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
+    
+    # update fighters
+
+    fighter_1.update()
+    fighter_2.update()
+
+    # draw fighters
+
+    fighter_1.draw(screen)
+    fighter_2.draw(screen)
+
+    #eventHandler
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    #update display
+
+    pygame.display.update()
+
+#Exit pygame
+
+pygame.quit()
